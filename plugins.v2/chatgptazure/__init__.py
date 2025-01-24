24f69569fa60914be3ed9ede7c060b222ed68efe
chaoji <jifc17@outlook.com>
2025-01-24 11:39:08 +0800

    fix bug

--- plugins.v2/chatgptazure/__init__.py
+++ plugins.v2/chatgptazure/__init__.py
@@ -4,10 +4,10 @@ from app.core.config import settings
 from app.core.event import eventmanager, Event
 from app.log import logger
 from app.plugins import _PluginBase
-from app.plugins.chatgpt.openaiazure import OpenAiAzure
+from app.plugins.chatgptazure.openai import OpenAi
 from app.schemas.types import EventType, ChainEventType
 
-class ChatGPTA(_PluginBase):
+class ChatGPTAzure(_PluginBase):
     # 插件名称
     plugin_name = "ChatGPTAzure"
     # 插件描述
@@ -21,7 +21,7 @@ class ChatGPTA(_PluginBase):
     # 作者主页
     author_url = "https://github.com/jxxghp"
     # 插件配置项ID前缀
-    plugin_config_prefix = "chatgptaz_"
+    plugin_config_prefix = "chatgptazure_"
     # 加载顺序
     plugin_order = 15
     # 可使用的用户级别
@@ -47,7 +47,7 @@ class ChatGPTA(_PluginBase):
             self._openai_key = config.get("openai_key")
             self._model = config.get("model")
             if self._openai_url and self._openai_key:
-                self.openai = OpenAiAzure(api_key=self._openai_key, api_url=self._openai_url,
+                self.openai = OpenAi(api_key=self._openai_key, api_url=self._openai_url,
                                      proxy=settings.PROXY if self._proxy else None,
                                      model=self._model,provider=self._openai_provider)
 
